e2b762d160aee87eeef52ab4ca14f6327203cadb
KalminX <noriabure@gmail.com>
2024-11-16 06:03:48 +0100

    fix: fixed minor issues

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,10 +12,7 @@ def validUTF8(data):
     Returns:
         Boolean denoting if all are valid utf-8 encodings
     """
-    num = True
     for number in data:
-        if (number >> 8) == 0:
-            num = True
-        else:
-            num = False
-    return num
+        if (number >> 8) != 0:
+            return False            
+    return True
